7ce10041609492f51ec7af7dbac948c28bd6e353
giovani <giovaniocan@gmail.com>
2023-03-30 15:39:38 -0300

    import stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
+import { styled } from "../styles"
+
+ const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  border: 0,
+  borderRadius: 8,
+  padding: '16px 16px',
+
+  span: {
+    fontWeight: 'bold',
+  },  
+ })
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>teste</span>
+      Enviar
+      </Button>
   )
 }
